ce67493f1de459631ff7949c43b48b617a41a4e1
Shashank <49742350+shashankvalgen@users.noreply.github.com>
2019-05-02 17:58:03 +0530

    Create package.js

---
+++ package.js
@@ -0,0 +1,30 @@
+{
+  "name": "node-js-sample",
+  "version": "0.1.0",
+  "description": "A sample Node.js app using Express 4",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js",
+    "test": "mocha --reporter tap"
+  },
+  "dependencies": {
+    "chai": "^3.5.0",
+    "chai-http": "^3.0.0",
+    "dotenv": "^4.0.0",
+    "ejs": "^2.5.5",
+    "express": "^4.0.0",
+    "mocha": "^3.3.0"
+  },
+  "engines": {
+    "ejs": "2.5.5"
+  },
+  "repository": {
+    "type": "git",
+    "url": "https://github.com/shashankvalgen/demoshashank1"
+  },
+  "keywords": [
+    "node",
+    "heroku",
+    "express"
+  ],
+} 
